e4f11d7987c05b2459abffc8493754253a224adf
Fuat ŞENGÜL <fuat@fuatsengul.com.tr>
2014-11-01 20:22:34 +0200

    1.0.7

--- package.js
+++ package.js
@@ -1,18 +1,20 @@
 Package.describe({
-    summary: "Fixed version",
-    version: "1.0.0",
+    name: 'fuatsengul:iron-router-auth',
+    summary: "Iron:Router Auth for 1.0",
+    version: "1.0.7",
     git: "https://github.com/fuatsengul/iron-router-auth"
     
 });
 
-Package.on_use(function(api){
-    api.versionsFrom('METEOR@0.9.0.1');
+Package.onUse(function(api) {
+    api.versionsFrom('1.0');
     api.use([
-    'iron:router@0.9.1', 'session@1.0.0', 'underscore@1.0.0'
+    'iron:router@1.0.0', 'session@1.0.0', 'underscore@1.0.0'
     ], 'client');
+    api.addFiles('lib/iron-router-auth.js', 'client');
+});
 
-    api.addFiles([
-        'LICENSE',
-        'lib/iron-router-auth.js'
-    ], 'client');
+
+Package.onTest(function(api) {
+  api.use('tinytest');
 });
